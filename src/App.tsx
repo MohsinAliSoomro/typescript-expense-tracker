96220b3a4f9dd41d93ad39a53ddf8d242b0246a3
MohsinAliSoomro <mohsinalisoomro23@gmail.com>
2020-08-16 15:42:31 +0500

    intal ant design

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,26 @@ import IncomeExpensesList from './components/incomeExpenseList'
 import InExForm from './components/in-ex-form'
 import { GlobalProvider } from './globalState/globalProvider'
 import 'bootstrap/dist/css/bootstrap.css'
+import 'antd/dist/antd.css';
+import { Row, Col } from 'antd';
 function App() {
   return (
     <GlobalProvider>
-      <div className="container-main">
-        <Head />
-        <Blnc />
-        <IncomeExpenses />
-        <p>History</p>
-        <hr />
-        <IncomeExpensesList />
-        <InExForm />
+      <div>
+        <Row>
+          <Col span={8}></Col>
+          <Col span={8}>
+            <Head />
+            <Blnc />
+            <IncomeExpenses />
+            <p>History</p>
+            <hr />
+            <IncomeExpensesList />
+            <InExForm />
+          </Col>
+          <Col span={8}></Col>
+        </Row>
+
       </div>
     </GlobalProvider>
   );
--- src/components/incomeExpenseList.tsx
+++ src/components/incomeExpenseList.tsx
@@ -2,12 +2,14 @@ import React, { useContext } from 'react';
 import { GlobalContext } from '../globalState/globalProvider';
 import { ITrans } from '../transType/tranType'
 import Transaction from './transaction'
-import Button from '@material-ui/core/Button';
+
+
+
 const IncomeExpensesList = () => {
     const { state } = useContext(GlobalContext)
+    
     return (
         <div className="in-ex-list">
-            <Button>click me</Button>
             <ul>
                 {state.transactions.map((item: ITrans) => {
                     return (
