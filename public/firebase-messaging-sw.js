9061d19a1714530fa2a88d823a8a7658ed6c8bca
MohsinAliSoomro <mohsinalisoomro23@gmail.com>
2020-09-09 13:08:19 +0500

    complete pwa and push notificatian

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,15 @@
+importScripts('https://www.gstatic.com/firebasejs/7.18.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/7.18.0/firebase-messaging.js');
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDBssBOwPrr7tyG3DK5r4jsguO95x5hlIs",
+    authDomain: "pushnotification-36bfa.firebaseapp.com",
+    databaseURL: "https://pushnotification-36bfa.firebaseio.com",
+    projectId: "pushnotification-36bfa",
+    storageBucket: "pushnotification-36bfa.appspot.com",
+    messagingSenderId: "309907246222",
+    appId: "1:309907246222:web:e074739c8d10f7b7b70ddf"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.messaging();
--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,21 @@ import IncomeExpensesList from "./components/incomeExpenseList";
 import InExForm from "./components/in-ex-form";
 import { GlobalProvider } from "./globalState/globalProvider";
 import "bootstrap/dist/css/bootstrap.css";
-import "antd/dist/antd.css";
+import { messaging } from "./sevices/firebase";
+
+Notification.requestPermission((permissions) => {
+  console.warn(permissions);
+  if (permissions === "granted") {
+    messaging
+      .getToken()
+      .then((currentToken) => {
+        console.log(currentToken);
+      })
+      .catch((err) => {
+        console.log("Something is wrong... " + err);
+      });
+  }
+});
 
 function App() {
   return (
